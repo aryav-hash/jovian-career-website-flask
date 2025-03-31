6de187a7cc226b88394f5aa4c9fef5ede7fc5db8
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-03-31 19:09:29 +0530

    Added a separate bootstrap file which contains the css bootstrap code and made the links in home.html working by showing the specific job details.

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
+  if not job:
+    return "Job not found", 404
   return render_template('jobpage.html', job=job)
 
 if __name__ == "__main__":
